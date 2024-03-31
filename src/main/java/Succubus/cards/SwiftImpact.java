3de8acf76e79d8345741ae518f5dd3c2931305bd
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-03-31 12:01:47 -0300

    Add SwiftImpact

---
+++ src/main/java/Succubus/cards/SwiftImpact.java
@@ -0,0 +1,63 @@
+package Succubus.cards;
+
+import Succubus.cards.abstracts.AbstractEasyCard;
+import Succubus.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.watcher.ChangeStanceAction;
+import com.megacrit.cardcrawl.actions.watcher.NotStanceCheckAction;
+import com.megacrit.cardcrawl.cards.red.BodySlam;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.stances.NeutralStance;
+import com.megacrit.cardcrawl.vfx.combat.EmptyStanceEffect;
+
+import static Succubus.MainModfile.makeID;
+
+public class SwiftImpact extends AbstractEasyCard {
+    public final static String ID = makeID(SwiftImpact.class.getSimpleName());
+
+    public SwiftImpact() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 5;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_HEAVY);
+        addToBot(new NotStanceCheckAction("Neutral", new VFXAction(new EmptyStanceEffect(p.hb.cX, p.hb.cY), 0.1F)));
+        addToBot(new ChangeStanceAction("Neutral"));
+    }
+
+    @Override
+    public void applyPowers() {
+        int base = baseDamage;
+        if (!(Wiz.adp().stance instanceof NeutralStance)) {
+            baseDamage *= 2;
+        }
+        super.applyPowers();
+        baseDamage = base;
+        isDamageModified = baseDamage != damage;
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        int base = baseDamage;
+        if (!(Wiz.adp().stance instanceof NeutralStance)) {
+            baseDamage *= 2;
+        }
+        super.calculateCardDamage(mo);
+        baseDamage = base;
+        isDamageModified = baseDamage != damage;
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return BodySlam.ID;
+    }
+}
