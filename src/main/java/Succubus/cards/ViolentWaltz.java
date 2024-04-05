51f376e77c6bda409f852590989d032745e8465b
Autumn Turner <75217888+MistressAlison@users.noreply.github.com>
2024-04-05 15:45:13 -0300

    Add Violent Waltz

---
+++ src/main/java/Succubus/cards/ViolentWaltz.java
@@ -0,0 +1,45 @@
+package Succubus.cards;
+
+import Succubus.actions.FinaleAction;
+import Succubus.cards.abstracts.AbstractEasyCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.red.Cleave;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Succubus.MainModfile.makeID;
+
+public class ViolentWaltz extends AbstractEasyCard {
+    public final static String ID = makeID(ViolentWaltz.class.getSimpleName());
+
+    public ViolentWaltz() {
+        super(ID, 2, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        baseDamage = damage = 12;
+        isMultiDamage = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        allDmg(AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+        addToBot(new FinaleAction(() -> allDmgTop(AbstractGameAction.AttackEffect.BLUNT_HEAVY)));
+    }
+
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        if (AbstractDungeon.player.hand.group.stream().noneMatch(c -> c != this)) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+        }
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(3);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Cleave.ID;
+    }
+}
